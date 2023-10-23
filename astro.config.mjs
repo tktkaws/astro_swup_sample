fd98a26f6d264806920f02bc7db2fbf7cf96fe1c
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-10-23 20:52:11 +0900

    Add: install @swup/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import swup from "@swup/astro";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    swup({
+      theme: "fade",
+      //   morph: "#nav",
+      //   parallel: true,
+    }),
+  ],
 });
